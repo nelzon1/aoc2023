dbb99df26524e96ba1e06d2f03dd5d94f20ec2a7
Jake Nelson <jake.nelzon@gmail.com>
2023-12-03 10:34:18 -0800

    Day 3 part 1

--- aoc2023_3/src/main.rs
+++ aoc2023_3/src/main.rs
@@ -1,6 +1,5 @@
 use std::error::Error;
 use std::fs::File;
-use std::fmt::Display;
 use std::io::{self, BufRead};
 use regex::Regex;
 
@@ -10,37 +9,93 @@ fn main() -> Result<(), Box<dyn Error>>{
     // we need index of each number
     // we calculate row and start/end (inclusive)
     // function will check the master array and see if there are any special chars in the surrounding cells
-    let re_number:Regex = Regex::new(r"(\d+)").unwrap();
+    let re_number:Regex = Regex::new(r"\d+").unwrap();
 
-    let mut master: Vec<Vec<char>>= vec![vec![]];
+    let mut master: Vec<Vec<char>>= vec![];
     let mut original: Vec<String> = Vec::new();
+    let mut part_total = 0;
+    let mut part_numbers:Vec<i32> = Vec::new();
 
 
-    let file_path = "input_debug.txt";
+    let file_path = "input.txt";
     // Open the file
     let file = File::open(file_path)?;
+    let mut row_num = 0;
 
-    // Create a buffered reader to read lines efficiently
+    // Build the dataset
     let reader = io::BufReader::new(file);
     for line in reader.lines() {
         let orig_line = line?;
-        if let captures = re_number.captures_iter(&orig_line) {
-            // Access captured groups using captures
-            for number in captures{
-                //println!("{}", number.extract().to_string());
+            master.push(orig_line.chars().collect());
+            original.push(orig_line);
+    }
+
+    // Iterate over the line
+    for line in original{
+        let captures = re_number.captures_iter(&line);
+        // Access captured groups using captures
+        for capture in captures{
+            for group in capture.iter(){
+                if let Some(matched) = group {
+                    let start = matched.start() as i32;
+                    let end = matched.end() as i32;
+                    if is_part_number(&master, row_num, start, end) {
+                        let result = matched.as_str().parse::<i32>().unwrap();
+                        part_total += result;
+                        part_numbers.push(result);
+                        println!("{}", "Part ".to_string() + matched.as_str() + " valid" );
+                    }
+                    else {
+                        println!("{}", "Part ".to_string() + matched.as_str() + " INVALID" );
+                    }
+                }
+                //println!("{}", "number.extract().to_string()");
+                // if let (match, match_info) = number.unwrap(); 
+                // println!("{}", number.extract());
             }
         }
-        master.push(orig_line.chars().collect());
-        original.push(orig_line);
+
+        
+        row_num += 1;
 
     }
 
-    println!("{}", "total power: ".to_string());
+    println!("{}", "part total : ".to_string() + &part_total.to_string());
     Ok(())
 }
 
 
-fn is_part_number(master:Vec<Vec<char>>, row:i32, start:i32, len: i32) -> bool {
-
-    return true;
+fn is_part_number(master:&Vec<Vec<char>>, row:i32, start:i32, end: i32) -> bool {
+    let special_chars:Vec<char> = vec!['@','#','$','%','&','*','-','=','+','/'];
+    let start_min = if start > 0 {start - 1} else {start};
+    let end_max = if end < master[row as usize].len() as i32 - 1 {end + 1} else {end};
+    // get row above (check if index is 0)
+    if row > 0 {
+        let test_chars = &master[(row - 1) as usize][start_min as usize..end_max as usize];
+        if test_chars.iter().any(|&element| special_chars.contains(&element)) {
+            return true;
+        }
+    }
+    // get row below (check if index = len-1)
+    if row < master.len() as i32 - 1 {
+        let test_chars = &master[(row + 1) as usize][start_min as usize..end_max as usize];
+        if test_chars.iter().any(|&element| special_chars.contains(&element)) {
+            return true;
+        }
+    }
+    // get left char (check start)
+    if start > 0 {
+        let test_char = &master[row as usize][(start - 1) as usize];
+        if special_chars.contains(test_char) {
+            return true;
+        }
+    }
+    // get right char (check end)
+    if end < master[row as usize].len() as i32 {
+        let test_char = &master[row as usize][(end) as usize];
+        if special_chars.contains(test_char) {
+            return true;
+        }
+    }
+    return false;
 }
